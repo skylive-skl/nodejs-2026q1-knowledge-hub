6e05dd8d26b9bf3439d76f354ae1ff1b2b81fd0e
Ismet <kadyrovismet1999@gmail.com>
2026-04-05 01:44:13 +0500

    feat: add config module and add common interfaces

---
+++ src/common/enums.ts
@@ -0,0 +1,13 @@
+export enum UserRole {
+  // 'admin' | 'editor' | 'viewer';
+  ADMIN = 'admin',
+  EDITOR = 'editor',
+  VIEWER = 'viewer',
+}
+
+export enum ArticleStatus {
+  //'draft' | 'published' | 'archived'
+  DRAFT = 'draft',
+  PUBLISHED = 'published',
+  ARCHIVED = 'archived',
+}
