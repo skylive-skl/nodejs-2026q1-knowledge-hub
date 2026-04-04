e7b75d28184cbc33b74968d84c9aeecffa757e2a
Ismet <kadyrovismet1999@gmail.com>
2026-04-05 01:26:19 +0500

    feat: add env global pipe and global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
